8c3ed56d24f4e33e26090cac71a7b581c4810848
Suchismita Panda <141360010+Suchismitap@users.noreply.github.com>
2026-01-28 13:21:31 +0530

    Update demo_react.jsx

--- demo_react.jsx
+++ demo_react.jsx
@@ -1,1 +1,17 @@
+import { useState } from "react";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="container">
+      <h1>🚀 Demo React App</h1>
+      <p>This app is running inside StackBlitz</p>
+
+      <button onClick={() => setCount(count + 1)}>
+        Clicked {count} times
+      </button>
+    </div>
+  );
+}
 
